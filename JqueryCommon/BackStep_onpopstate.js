c3bec7cc149751f231982048403b4a4fef1a5ff9
hunjeongPark <88864771+hunjeongPark@users.noreply.github.com>
2022-03-21 17:36:04 +0900

    Create BackStep_onpopstate.js
    One Page Section to Homepage, Backstep To CurrentPage CallBack Function

---
+++ JqueryCommon/BackStep_onpopstate.js
@@ -0,0 +1,32 @@
+//Jquery - HTML5, Chrome, FireFox, Edge .... > onpopstate Backstep Controll To current Page
+
+window.onpopstate = function () {
+    var pageUrl = location.href;
+    alert(pageUrl);
+    
+    //Controll To CurrentPage And Use Id For Step div match
+    if(prevUrl.indexOf([currentPageName]) < 0 && $("#step1").hasClass("hide")) {
+		history.back();
+	} else {
+    //step1,2,3,4.... Step Show the One Page Jquery Script
+		UIHandler.showStep1();
+	}
+};
+
+// UIHandler Controll show step
+let UIHandler = {
+	showStep1 : function() {
+		this.hideAllStep();
+		$("#step1").removeClass("hide");
+	},
+	showStep2 : function() {
+		this.hideAllStep();
+		$("#step2").removeClass("hide");
+	},
+	hideAllStep : function() {
+		$(".step").addClass("hide");
+	}
+}
+
+//history.pushstate(state, title, url) > 뒤로가기 클릭 시 현재 페이지의 window.onpopstate를 호출하도록 지정
+history.pushstate(null, null, location.href); // => 제자리 호출을 위하여 현재 location.href URL을 넣어줍니다. 호출 위치는 뒤로가기 클릭을 하는 순간에 위치시켜줍니다.
